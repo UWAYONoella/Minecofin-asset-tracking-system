d4b0ac8eb743d794ebe32c6c5a8fbe55271c5b01
Noella UWAYO <noella.uwayo@minecofin.gov.rw>
2026-06-01 11:46:17 +0200

    Created dashboard page

--- backend/assets/views.py
+++ backend/assets/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Computer, Vehicle, Projector, Employee
 
-# Create your views here.
+
+def dashboard(request):
+    context = {
+        "computers": Computer.objects.count(),
+        "vehicles": Vehicle.objects.count(),
+        "projectors": Projector.objects.count(),
+        "employees": Employee.objects.count(),
+    }
+
+    return render(request, "assets/dashboard.html", context)
